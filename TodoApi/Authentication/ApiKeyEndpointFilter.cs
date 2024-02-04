37808dd5d8fb0ef11f8c6d602042413194372f95
Mirek <40789254+mirosuav@users.noreply.github.com>
2024-02-04 22:05:56 +0100

    SecretManager used for API_KEY

--- TodoApi/Authentication/ApiKeyEndpointFilter.cs
+++ TodoApi/Authentication/ApiKeyEndpointFilter.cs
@@ -4,10 +4,12 @@ namespace TodoApi.Authentication;
 public class ApiKeyEndpointFilter : IEndpointFilter
 {
     private readonly IConfiguration _configuration;
+    private readonly ILogger<ApiKeyEndpointFilter> logger;
 
-    public ApiKeyEndpointFilter(IConfiguration configuration)
+    public ApiKeyEndpointFilter(IConfiguration configuration, ILogger<ApiKeyEndpointFilter> logger)
     {
         _configuration = configuration;
+        this.logger = logger;
     }
 
     public async ValueTask<object?> InvokeAsync(
@@ -19,8 +21,13 @@ public class ApiKeyEndpointFilter : IEndpointFilter
         {
             return TypedResults.Unauthorized();
         }
-        var _apiKey = _configuration.GetValue<string>("API_KEY") 
-            ?? throw new ApplicationException("Invalid configuration: no API KEY defined!");
+        
+        var _apiKey = _configuration.GetValue<string>("API_KEY");
+        if (_apiKey is null or [])
+        {
+            logger.LogError("Invalid configuration: no API_KEY environment variable defined!");
+            throw new ApplicationException("Invalid application configuration");            
+        }
 
         if (!_apiKey.Equals(requestApiKey))
         {
